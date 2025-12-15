6b910208e4a3b818649f327a7c04c7731460564b
Vinicius Barbosa <vinicius.vrsb@gmail.com>
2025-12-15 14:22:53 -0300

    tratamento de erros

--- src/maioridade/MaiorIdade.java
+++ src/maioridade/MaiorIdade.java
@@ -6,34 +6,63 @@ import java.util.Scanner;
 public class MaiorIdade {
     public static void main(String[] args) {
         String nome;
-        int idade;
+        String idade;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Me informe o seu nome: ");
         nome = scanner.nextLine();
 
         System.out.println("Me informe sua idade: ");
-        idade = scanner.nextInt();
-        if (idade < 0){
+        idade = scanner.nextLine();
+
+
+        try{
+            int idadeInt = Integer.parseInt(idade);
+            if (idadeInt < 0){
+                System.out.println("Número inválido!!");
+                return;
+            }
+            if (idadeInt >= 18){
+                System.out.println(nome + ", você é maior de idade!");
+            } else {
+                System.out.println(nome + ", você é menor de idade");
+            }
+
+            if (idadeInt <= 10){
+                System.out.println("Parabéns " + nome + " você é uma criança! ");
+            } else if (idadeInt > 10 && idadeInt <=18) {
+                System.out.println("Parabéns " + nome + " você está na adolescência!");
+            } else if (idadeInt > 18 && idadeInt <= 60) {
+                System.out.println("Parabéns " + nome + " você é um adulto!");
+            }else {
+                System.out.println("Parabéns " + nome + " você é um idoso, está na melhor idade!");
+            }
+
+        } catch (NumberFormatException x){
+            System.out.println("Digite um número válido!");
+        }
+
+
+        /* if (idadeInt < 0){
             System.out.println("Número inválido!!");
             return;
         }
 
-        if (idade >= 18){
+        if (idadeInt >= 18){
             System.out.println(nome + ", você é maior de idade!");
         } else {
             System.out.println(nome + ", você é menor de idade");
         }
 
-        if (idade <= 10){
+        if (idadeInt <= 10){
             System.out.println("Parabéns " + nome + " você é uma criança! ");
-        } else if (idade > 10 && idade <=18) {
+        } else if (idadeInt > 10 && idade <=18) {
             System.out.println("Parabéns " + nome + " você está na adolescência!");
-        } else if (idade > 18 && idade <= 60) {
+        } else if (idadeInt > 18 && idade <= 60) {
             System.out.println("Parabéns " + nome + " você é um adulto!");
         }else {
             System.out.println("Parabéns " + nome + " você é um idoso, está na melhor idade!");
-        }
+        } */
 
     }
 }
