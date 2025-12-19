2d7e851c48ce3b4b03748689335e1b6fa912fa27
Vinicius Barbosa <vinicius.vrsb@gmail.com>
2025-12-18 21:17:59 -0300

    Atualização do exercício do sistema de notas

--- src/sistemadenotas/SistemaDeNotas.java
+++ src/sistemadenotas/SistemaDeNotas.java
@@ -7,24 +7,37 @@ import java.util.Scanner;
 public class SistemaDeNotas {
     public static void main(String[] args) {
         int numAlunos;
-        String nomeAluno;
-        double nota = 0, media;
-        ArrayList<String> nomes = new ArrayList<>();
-        ArrayList<Double> notas = new ArrayList<>();
+        String nomeAluno, alunoMaiorNota = "";
+        double nota = 0, media, soma = 0, maiorNota = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("SISTEMA DE NOTAS:");
         System.out.println("DIGITE QUANTOS ALUNOS SÃO: ");
         numAlunos = scanner.nextInt();
+        scanner.nextLine();
+
 
         for (int i=0; i<numAlunos; i++){
             System.out.println("DIGITE O NOME DO ALUNO: ");
             nomeAluno = scanner.nextLine();
-            nomes.add(nomeAluno);
             System.out.println("DIGITE A NOTA DO ALUNO: ");
             nota = scanner.nextDouble();
-            notas.add(nota);
+            scanner.nextLine();
+
+            soma += nota;
+
+            if(i==0){
+                maiorNota = nota;
+                alunoMaiorNota = nomeAluno;
+            }else if (nota > maiorNota){
+                maiorNota = nota;
+                alunoMaiorNota = nomeAluno;
+            }
+
+
         }
-        media = nota/numAlunos;
+        media = soma/numAlunos;
+        System.out.println("A média da nota dos alunos é: " + media);
+        System.out.println("O aluno com a maior nota foi: " + alunoMaiorNota);
     }
 }
